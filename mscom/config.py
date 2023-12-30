9a7f6cb0c06b39b622f5e7b02f73f86b6c9bb8c9
Vitaly Legchilkin <vlegchilkin@gmail.com>
2023-12-30 13:55:07 +0100

    change reload process

--- mscom/config.py
+++ mscom/config.py
@@ -1,10 +1,13 @@
+from pathlib import Path
+from typing import List
+
 from pydantic_settings import BaseSettings
 
 
 class MicroserviceSettings(BaseSettings):
     server_port: int = 8080
     # enables auto-reload based on uvicorn
-    enable_reload: bool = False
+    reload_dirs: List[Path] = []
 
 
 class LogSettings(BaseSettings):
